742aa035007005bed2134f9e886030349b89d8fe
lee heechan <leeheechan1@gmail.com>
2025-04-14 20:18:06 +0900

    변경 사항
    Flask 기본 앱 구성 및 Dockerfile 추가.
    Dockerfile로 만들 Docker Image에 불필요한 요소들을 무시하는 .dockerignore 추가.

--- aws/ecs/simple-web/app/routes.py
+++ aws/ecs/simple-web/app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
+
+import platform
+import socket
+
+
+fqdn = socket.getfqdn()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world'
+    return render_template('index.html', fqdn = fqdn)
