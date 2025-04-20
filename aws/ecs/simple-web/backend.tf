e0fae7a4c2a0c124d746f9920a76b37f92b92dad
lee heechan <leeheechan1@gmail.com>
2025-04-21 00:19:07 +0900

    변경 사항 .gitignore에 제외할 파일 추가 Terraform의 Backend 및 Provider 구체적으로 구현

--- aws/ecs/simple-web/backend.tf
+++ aws/ecs/simple-web/backend.tf
@@ -6,4 +6,11 @@ terraform {
       name = "simple-web"
     }
   }
+# Set provider AWS. Version 5.95.0
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.95.0"
+    }
+  }
 }
