77e9d2e1d3929d102e83d4acce8b0276b8278b7f
lee heechan <leeheechan1@gmail.com>
2025-04-25 23:42:53 +0900

    변경 사항 AWS IAM 서비스를 사용하여 ECS에 필요한 IAM 역할, 정책 문서 생성

--- aws/ecs/simple-web/main.tf
+++ aws/ecs/simple-web/main.tf
@@ -13,3 +13,33 @@ resource "aws_ecs_cluster" "web_server" {
     "IaCTool" = "Terraform"
   }
 }
+
+# IAM 정책 문서
+# ECS Task Execution Role. 목표: AWS ECS가 AWS ECR에 적법한 접근 권한을 행사하여 Image를 Pull 할 수 있게 한다.
+resource "aws_iam_role" "allow_ecs" {
+  name               = "Simple-Web"
+  assume_role_policy = file("iam/role/ECS-AssumeRole.json")
+
+  description = "Let ECS to assume Role"
+  tags = {
+    "IaCTool" = "Terraform"
+  }
+}
+
+resource "aws_iam_policy" "allow_ecs" {
+  name   = "Simple-Web-Access"
+  policy = file("iam/policy/ECS-AllowECRAccess.json")
+}
+
+resource "aws_iam_role_policy_attachment" "attach_document" {
+  role       = aws_iam_role.allow_ecs.name
+  policy_arn = aws_iam_policy.allow_ecs.arn
+}
+## 정책이 만들어지지 않아 잠시 주석 처리
+#Create ECS Task Definition
+# resource "aws_ecs_task_definition" "simple_web" {
+#   family = "simple-web"
+#   container_definitions = file("ecs/task_definitions/simple_web.json")
+
+#   execution_role_arn = ""
+# }
