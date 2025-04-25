dba72195eb31478c51f049d9b88f2c15da8bd105
lee heechan <leeheechan1@gmail.com>
2025-04-25 17:29:18 +0900

    변경 사항 main.tf: ECS Cluster 생성
    readme.md 추가

--- aws/ecs/simple-web/main.tf
+++ aws/ecs/simple-web/main.tf
@@ -1,5 +1,15 @@
 # Create AWS ECR Private Registry
-resource "aws_ecr_repository" "simple-web" {
-  name = "simple-web"
+resource "aws_ecr_repository" "simple_web" {
+  name = "cloud-projects"
 
+}
+
+# Create ECS Cluster.
+resource "aws_ecs_cluster" "web_server" {
+  name = "Simple-Web-Server"
+  # Add Tag.
+  tags = {
+    # This Cluster is created & managed using Terraform
+    "IaCTool" = "Terraform"
+  }
 }
